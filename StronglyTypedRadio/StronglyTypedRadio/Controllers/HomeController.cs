fc0e6b91693575866d351feb30e1d109c8cc49b4
Shyju <connectshyju@gmail.com>
2012-11-20 21:25:12 -0600

    Initial commit
    Sample code for Radio button list in ASP.NET MVC3

---
+++ StronglyTypedRadio/StronglyTypedRadio/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewModels;
+
+namespace StronglyTypedRadio.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var evalVM = new Evaluation();
+
+            //the below is hardcoded for DEMO. you may get the data from some  
+            //other place and set the questions and answers
+
+            var q1 = new Question { ID = 1, QuestionText = "What is your favourite language" };
+            q1.Answers.Add(new Answer { ID = 12, AnswerText = "PHP" });
+            q1.Answers.Add(new Answer { ID = 13, AnswerText = "ASP.NET" });
+            q1.Answers.Add(new Answer { ID = 14, AnswerText = "Java" });
+            evalVM.Questions.Add(q1);
+
+            var q2 = new Question { ID = 2, QuestionText = "What is your favourite DB" };
+            q2.Answers.Add(new Answer { ID = 16, AnswerText = "SQL Server" });
+            q2.Answers.Add(new Answer { ID = 17, AnswerText = "MyQL" });
+            q2.Answers.Add(new Answer { ID = 18, AnswerText = "Oracle" });
+            evalVM.Questions.Add(q2);
+
+            return View(evalVM);           
+        }
+
+      
+        [HttpPost]
+        public ActionResult Index(Evaluation model)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var q in model.Questions)
+                {
+                    var qId = q.ID;
+                    var selectedAnswer = q.SelectedAnswer;
+                    // Save the data 
+
+                }
+                return RedirectToAction("ThankYou"); //PRG Pattern
+            }
+            //reload questions
+            return View(model);
+        }
+
+
+
+
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ StronglyTypedRadio/StronglyTypedRadio/ViewModels/EvaluationViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ViewModels
+{
+    public class Question
+    {
+        public int ID { set; get; }
+        public string QuestionText { set; get; }
+        public List<Answer> Answers { set; get; }
+        [Required]
+        public int SelectedAnswer { set; get; }
+        public Question()
+        {
+            Answers = new List<Answer>();
+        }
+    }
+    public class Answer
+    {
+        public int ID { set; get; }
+        public string AnswerText { set; get; }
+    }
+    public class Evaluation
+    {
+        public List<Question> Questions { set; get; }
+        public Evaluation()
+        {
+            Questions = new List<Question>();
+        }
+    }
+}
